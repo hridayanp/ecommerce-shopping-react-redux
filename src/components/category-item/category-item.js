3a3ae56730b874543bcf1828aa57a481abe14d70
hridayanp <hridayanphukan123@gmail.com>
2022-04-06 21:24:23 +0530

    setup project and added basic styles.

---
+++ src/components/category-item/category-item.js
@@ -0,0 +1,17 @@
+import './category-item.styles.scss';
+
+export const CategoryItem = ({category}) => {
+    const {title, imageUrl} = category;
+
+    return (
+        <div className="category-container">
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }} />
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop now</p>
+            </div>
+        </div>
+    )
+}

5cf63f0ea95e2dbb80f80dd3b885d58f97e00e4b
hridayanp <hridayanphukan123@gmail.com>
2022-05-28 20:19:03 +0530

    added cart icon and cart dropdown functionality.

--- src/routes/navigation/navigation.js
+++ src/routes/navigation/navigation.js
@@ -4,9 +4,13 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { UserContext } from '../../contexts/user.context';
+import { CartIcon } from '../../components/cart-icon/cart-icon';
+import { CartDropdown } from '../../components/cart-dropdown/cart-dropdown';
+import { CartContext } from '../../contexts/cart.context';
 
 export const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
     // console.log(currentUser);
 
 
@@ -33,7 +37,11 @@ export const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+
+                    <CartIcon />
                 </div>
+
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
